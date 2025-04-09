4868b7f346c94f82bc36bce5e706537c705c6494
Anders <86270741+annjersm@users.noreply.github.com>
2025-04-09 23:04:01 +0200

    Update type.h

--- src/util/type.h
+++ src/util/type.h
@@ -1,6 +1,21 @@
 #pragma once
+// #################################################################|#########
+// #################################################################|# (¬_¬) #
+// #################################################################|#########
+#include <stdint.h>
 
+// Signed int
+typedef int8_t       int8;
+typedef int16_t      int16;
+typedef int32_t      int32;
+typedef int64_t      int64;
+
+// Unsigned int
 typedef unsigned int Uint;
+typedef uint8_t      Uint8;
+typedef uint16_t     Uint16;
+typedef uint32_t     Uint32;
+typedef uint64_t     Uint64;
 
 #define PI 3.14159265359
 #define GetDegrees(r) (r * (180 / PI))
